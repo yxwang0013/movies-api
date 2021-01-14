8e3d9dede2c4d6772118eba1ad619f7fdfc5ad53
yuxuan wang <2879296442@qq.com>
2021-01-14 21:04:22 +0000

    Week 9 Error HAndling

--- api/movies/index.js
+++ api/movies/index.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import {
-  getMovies, getMovie, getMovieReviews
+    getMovies, getMovie, getMovieReviews
 } from '../tmdb-api';
 
 const router = express.Router();
 
-router.get('/', (req, res,next) => {
-  getMovies().then(movies => res.status(200).send(movies));
+router.get('/', (req, res, next) => {
+    getMovies().then(movies => res.status(200).send(movies)).catch((error) => next(error));
 });
 
 router.get('/:id', (req, res, next) => {
-  const id = parseInt(req.params.id);
-  getMovie(id).then(movie => res.status(200).send(movie));
+    const id = parseInt(req.params.id);
+    getMovie(id).then(movie => res.status(200).send(movie)).catch((error) => next(error));
 });
 
 router.get('/:id/reviews', (req, res, next) => {
-  const id = parseInt(req.params.id);
-  getMovieReviews(id).then(reviews => res.status(200).send(reviews));
+    const id = parseInt(req.params.id);
+    getMovieReviews(id)
+        .then(reviews => res.status(200).send(reviews))
+        .catch((error) => next(error));
 });
 
 export default router;
