80acf7ce7a9a1465955018c5eaa4dcd8351c19b4
yuxuan wang <2879296442@qq.com>
2021-01-15 20:23:05 +0000

    Topic 10 - Passport

--- index.js
+++ index.js
@@ -3,43 +3,45 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import './db';
-import { loadUsers } from './seedData';
+import {loadUsers} from './seedData';
 import usersRouter from './api/users';
 import genresRouter from './api/genres';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 
 dotenv.config();
 
 const app = express();
-const errHandler = (err, req, res, next) => {
-    /* if the error in development then send stack trace to display whole error,
-    if it's in production then just send error message  */
-    if (process.env.NODE_ENV === 'production') {
-        return res.status(500).send(`Something went wrong!`);
-    }
-    res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
-};
-
 const port = process.env.PORT;
-
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
 if (process.env.SEED_DB) {
-    loadUsers();
+  loadUsers();
 }
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(session({
-    secret: 'ilikecake',
-    resave: true,
-    saveUninitialized: true
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
 }));
+
+
 app.use(express.static('public'));
-app.use('/api/movies', authenticate, moviesRouter);
+
+app.use(passport.initialize());
+
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/genres', genresRouter);
 
 app.use(errHandler);
-
 app.listen(port, () => {
-    console.info(`Server running at ${port}`);
+  console.info(`Server running at ${port}`);
 });
--- seedData/index.js
+++ seedData/index.js
@@ -14,11 +14,11 @@ const users = [
 // deletes all user documents in collection and inserts test data
 export async function loadUsers() {
   console.log('load user Data');
-    try {
-      await userModel.deleteMany();
-      await users.forEach(user => userModel.create(user));
-      console.info(`${users.length} users were successfully stored.`);
-    } catch (err) {
-      console.error(`failed to Load user Data: ${err}`);
-    }
+  try {
+    await userModel.deleteMany();
+    await users.forEach(user => userModel.create(user));
+    console.info(`${users.length} users were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load user Data: ${err}`);
   }
+}
