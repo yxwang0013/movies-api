01bbded2e0972324441097fc4f613e0afb40d7bb
yuxuan wang <2879296442@qq.com>
2021-01-15 16:47:44 +0000

    Week 9 User Favourites

--- api/users/index.js
+++ api/users/index.js
@@ -9,8 +9,8 @@ router.get('/', (req, res) => {
 });
 
 // register
-router.post('/', (req, res ,next) => {
-    User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
+router.post('/', (req, res, next) => {
+    User.create(req.body).then(user => res.status(200).json({ success: true, token: "FakeTokenForNow" })).catch(next);
 });
 
 // Update a user
@@ -23,4 +23,29 @@ router.put('/:id', (req, res, next) => {
     })
         .then(user => res.json(200, user)).catch(next);
 });
+
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = { username: req.params.userName };
+    if (newFavourite && newFavourite.id) {
+        User.find(query).then(
+            user => {
+                (user.favourites) ? user.favourites.push(newFavourite) : user.favourites = [newFavourite];
+                User.findOneAndUpdate(query, { favourites: user.favourites }, {
+                    new: true
+                }).then(user => res.status(201).send(user));
+            }
+        ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user");
+    }
+});
+
+router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find({ username: user }).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+});
+
 export default router;
